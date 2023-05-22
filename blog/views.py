3767c27751a611410354ebd623b6c9d2a0ac7b6b
jtk134 <jtk134@khu.ac.kr>
2023-05-23 01:32:05 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def yun_dongju(request):
+    return render(request, 'blog/yun_dongju.html', {})
