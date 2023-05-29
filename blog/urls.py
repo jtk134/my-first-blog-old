e36ca5aa6580dfcd79a175147128577032bba20a
jtk134 <jtk134@khu.ac.kr>
2023-05-29 23:58:50 +0900

    Design CSS

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
 from . import views
 
-"""
 urlpatterns = [
 path('', views.post_list, name='post_list'),
-]
-"""
-
-urlpatterns = [
-path('', views.yun_dongju, name='yun_dongju'),
 ]
